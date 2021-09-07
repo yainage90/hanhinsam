be958f05fdb30f1c26306a71a1f56d8d3972cb5e
yaincoding <yaincoding@gmail.com>
2021-09-08 00:31:06 +0900

    initial commit

---
+++ src/main/java/com/yaincoding/hanhinsam/HanhinsamApplication.java
@@ -0,0 +1,13 @@
+package com.yaincoding.hanhinsam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanhinsamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanhinsamApplication.class, args);
+	}
+
+}
